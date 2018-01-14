408fb6c437a5894331390b76502f6fbfb4a01451
Ryan Vlaming <ryanvlaming@Ryans-MacBook-Pro.local>
2018-01-14 21:12:54 +0100

    add simple module loading for app/ folders

--- app/index.js
+++ app/index.js
@@ -1,21 +1,28 @@
 const Kube = require('kube-f');
 const Promise = require('bluebird');
 
+const fs = Promise.promisifyAll(require('fs'), {suffix: 'Promise'});
+const singletonKubeInstance = new Kube();
+
+
 export default function appInit() {
-    const singletonKubeInstance = new Kube();
-    const fs = Promise.promisifyAll(require('fs'), {suffix: 'Promise'});
+
     //load services
-    singletonKubeInstance = fs.readdirPromise('./app')
-        .filter(file => !file.includes('.'))
-        .map(function initDir(dirName) {
-            
+    fs.readdirPromise('./app')
+        .filter(nonDirectories)
+        .map(mountBaseDirectory)
+}
+
+function mountBaseDirectory(directoryName) {
+    const directoryItems = fs.readdirPromise(`./app/${directoryName}`)
+        .filter(nonDirectories)
+        .map(function handleBaseDirectoryItem(directoryItem) {
+            const mountableDirectoryItem = require(`./${directoryName}/${directoryItem}`).default;
+            singletonKubeInstance.mountModule(mountableDirectoryItem);
         })
 }
 
-function initDir(dirName) {
-    const dirList = fs.readdirPromise(`./app/${dirName}`)
-        .map(loadModule)
-}
+const nonDirectories = file => !file.includes('.')
 
 /*
     Kube instance.
---
+++ app/services/exampleService/index.js
@@ -0,0 +1,3 @@
+export default function exampleService() {
+    console.log('meme');
+}
