965cb808a89702c0b22c78457a7d2f272a561589
Ryan Vlaming <ryanvlaming@Ryans-MacBook-Pro.local>
2018-01-28 19:50:40 +0100

    add simple JWT middleware

--- app/middlewares/jwt-middlewares/index.js
+++ app/middlewares/jwt-middlewares/index.js
@@ -1,16 +1,18 @@
-import jwt from 'jsonwebtoken';
+import jwt, { verify } from 'jsonwebtoken';
 import Promise from 'bluebird';
 
 const loginRoute = 'login';
 
 export default function JWTMiddlewares(kube, server) {
+    Promise.promisifyAll(jwt, { suffix: 'Promise' })
     server.use(function jwtMiddleware(req, res, next) {
         //check if this is sent to the login route
         if(isLoginRoute(req)) {
-            kube.logger.trace('request is login');
+            kube.logger.trace('handling non JWT route');
             next();
         } else {
-            kube.logger.trace('request is not login')
+            kube.logger.trace('handling jwt protected route')
+            verifyJWTToken(req, res);
         }
     })
 }
@@ -18,4 +20,20 @@ export default function JWTMiddlewares(kube, server) {
 function isLoginRoute(req) {
     const urlParams = req.url.split('/');
     return urlParams[urlParams.length-1].includes(loginRoute);
+}
+
+function verifyJWT(req, res) {
+    if(!req.header('authorization')) {
+        res.send(403, 'No JWT token provided')
+    }
+
+    const token = req.header('authorization');
+    const secret = process.env.JWT_SECRET;
+    return verifyPromise(token, secret)
+        .then(function handleVerifyResult(data, err) {
+            return err ? false : true;
+        })
+        .catch(function handleJWTError(err) {
+            res.send(403, 'Error verifying jwt token');
+        })
 }
