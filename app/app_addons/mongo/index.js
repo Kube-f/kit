59b86d2e9ab3bccb78efa5219445c91f134e8e32
Ryan Vlaming <ryanvlaming@Ryans-MacBook-Pro.local>
2018-02-03 13:31:54 +0100

    add simple mongodb connector

--- app/app_addons/mongo/index.js
+++ app/app_addons/mongo/index.js
@@ -1,8 +1,21 @@
+
+import Promise from 'bluebird';
+const mongoClient = Promise.promisifyAll(require('mongodb').MongoClient, {suffix: 'Promise'});
+
 export default function mongoAddon(kube) {
   const mongoNamespace = kube.namespace('mongo');
 
   mongoNamespace.def(function setupConnection() {
     kube.logger.trace('setting up mongo connection');
-    return 0;
+    
+    const url = process.env.MONGO_URL;
+
+    return mongoClient.connectPromise(url)
+      .then(function handleConnection(err, { db }) {
+        if(err) {
+          kube.logger.error({err}, 'Error while connecting to mongodb');
+        }
+        return db();
+      })
   });
 }
--- app/services/loginService/index.js
+++
@@ -1,7 +0,0 @@
-export default function loginService(kube, server) {
-    const loginService = kube.namespace('loginNamespace');
-
-    server.post('/v1/login', function handleLoginRoute(req, res) {
-        res.send(200, {});
-    })
-}
