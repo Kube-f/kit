51f39e1b81208d49791343cb0743e93e15e7dc0b
Ryan Vlaming <ryanvlaming@Ryans-MacBook-Pro.local>
2018-03-14 11:56:52 +0100

    refactor jwt

--- app/middlewares/jwt-middlewares/index.js
+++ app/middlewares/jwt-middlewares/index.js
@@ -9,22 +9,19 @@ export default function JWTMiddlewares(kube, server) {
     const jwtMiddleware = kube.namespace('jwt');
 
     jwtMiddleware.def(function verifyJWTToken(req, res, next) {
-        if(!req.header('authorization')) {
+        const token = req.header('authorization').split(' ')[1];
+        if(!token || token == '') {
             const errorMessage = 'No JWT token provided';
             kube.logger.error(errorMessage);
             res.send(403, errorMessage);
             return;
         }
     
-        const token = req.header('authorization').split(' ')[1];
         const secret = process.env.JWT_SECRET;
     
         return jwt.verifyPromise(token, secret)
             .then(function handleVerifyResult(data, err) {
                 next()
             })
-            .catch(function handleJWTError(err) {
-                res.send(403, {err, msg: 'Error verifying jwt token'});
-            });
     });
 }
